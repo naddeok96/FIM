dc31f256fe04d71b1da93d03148ddc74a0d36778
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-02-07 03:05:22 -0800

    calculated gradients wrt input

--- One_Step_Spectral_Attack.py
+++ One_Step_Spectral_Attack.py
@@ -8,6 +8,7 @@ Input: input sample x, corresponding labels y, a deep learning model with the ou
 '''
 import torch
 import operator
+from torch.autograd import Variable
 
 class OSSA:
 
@@ -22,34 +23,44 @@ class OSSA:
         self.criterion = torch.nn.CrossEntropyLoss()
         self.soft_max = torch.nn.Softmax(dim = 1)
 
-        self.soft_max_output, self.gradients, self.loss = self.get_outputs(self.net, 
+        self.soft_max_output, self.image_gradients, self.loss = self.get_outputs(self.net, 
                                                                            self.image,
                                                                            self.label)
 
-        #self.attack_perturbation, self.max_eigen_value = get_attack_elements()
-        print(self.get_attack_elements())
+        self.attack_perturbation, self.attack_perturbation_show = self.get_attack_elements()
         
     def get_outputs(self, net,
                           image, 
                           label):
 
+        image = Variable(image, requires_grad = True)
         output = net(image)
         soft_max_output = self.soft_max(output)
 
         loss = self.criterion(output, label.unsqueeze(0))
-        loss.backward()
+        loss.backward(retain_graph = True)
 
-        gradients = {}
-        for name, param in net.named_parameters():
-            gradients[name] = operator.attrgetter(name + '.grad')(net)
+        image_gradients = image.grad.data
             
-        return soft_max_output, gradients, loss.item()
+        return soft_max_output, image_gradients, loss.item()
 
     def get_attack_elements(self):
 
+        # Initialize Attack
         attack_perturbation = torch.rand(self.image.size())
+        attack_perturbation_show = attack_perturbation.unsqueeze(0)
+
+        alias_table = [self.soft_max_output]
+
+        '''
+        converged = False
+        while converged == False:
+            
+        '''
         
-        return attack_perturbation.size()
+        return attack_perturbation, attack_perturbation_show
+
+    
 
     
         
