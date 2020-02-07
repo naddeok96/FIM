903d8e82bb7f51c64389cab01d743c88d7bb2104
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-02-07 13:30:44 -0800

    made names .py names lowercase

--- One_Step_Spectral_Attack.py
+++ One_Step_Spectral_Attack.py
@@ -2,7 +2,6 @@
 This class implements the One Step Spectral Attack as formulated in the paper
 "The Adversarial Attack and Detection under the Fisher Information Metric"
 '''
-
 import torch
 import operator
 from torch.autograd import Variable
