fcfa84774d4895621a8091079d13d65782867a18
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-02-12 21:37:57 -0800

    changed gpu device

--- one_step_spectral_attack.py
+++ one_step_spectral_attack.py
@@ -21,7 +21,7 @@ class OSSA:
             self.label = label
         else:
              self.net   = net.cuda()
-             self.image = Variable(image, requires_grad = True).cuda()
+             self.image = Variable(image.cuda(), requires_grad = True)
              self.label = label.cuda()
 
         self.CONVERGE_LIMIT = CONVERGE_LIMIT
