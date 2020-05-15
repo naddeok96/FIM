763ae76748a076b50c0c762ebfc3a1bcae19607c
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-05-15 08:08:35 -0700

    set up fst lay rand lenet

--- model_trainer.py
+++ model_trainer.py
@@ -3,10 +3,11 @@ This script will train a model and save it
 '''
 # Imports
 import torch
-from rand_lenet import RandLeNet
-from first_layer_unitary_lenet import FstLayUniLeNet
-from unitary_lenet import UniLeNet
-from adjustable_lenet import AdjLeNet
+from models.classes.rand_lenet import RandLeNet
+from models.classes.first_layer_unitary_lenet import FstLayUniLeNet
+from models.classes.first_layer_rand_lenet import FstLayRandLeNet
+from models.classes.unitary_lenet import UniLeNet
+from models.classes.adjustable_lenet import AdjLeNet
 from data_setup import Data
 from academy import Academy
 
@@ -20,10 +21,10 @@ seed = 1
 if gpu == True:
     import os
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 torch.manual_seed(seed)
-net = FstLayUniLeNet(set_name = set_name,
+net = FstLayRandLeNet(set_name = set_name,
                         gpu = gpu,
                         num_kernels_layer3 = 100)
 
@@ -43,7 +44,7 @@ print(accuracy)
 # Save Model
 if save_model:
     # Define File Names
-    filename  = "mnist_fstlay_unilenet_w_acc_" + str(int(round(rand_accuracy * 100, 3))) + ".pt"
+    filename  = "mnist_fstlay_randlenet_w_acc_" + str(int(round(rand_accuracy * 100, 3))) + ".pt"
     
     # Save Models
     torch.save(academy.net.state_dict(), filename)
