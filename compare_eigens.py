44de4f04e31050aecc1a5ba4381e0593ff5b9e2a
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-20 00:30:17 -0700

    initial commit

---
+++ compare_eigens.py
@@ -0,0 +1,61 @@
+# Imports
+from models.classes.first_layer_unitary_lenet   import FstLayUniLeNet
+from models.classes.adjustable_lenet import AdjLeNet
+from adversarial_attacks import Attacker
+from prettytable import PrettyTable, ALL
+from data_setup import Data
+from academy import Academy
+import numpy as np
+
+# Hyperparameters
+set_name = "MNIST"
+gpu = False
+num_images = 2
+eig_vec_head_size = 3
+
+# Initialize data
+data = Data(gpu, set_name)
+
+# LeNet
+lenet = AdjLeNet(set_name = set_name,
+                 pretrained_weights_filename = 'models/pretrained/mnist_fstlay_uni_const_lenet_w_acc_95.pt')
+
+# UNet
+Unet = FstLayUniLeNet(set_name = set_name,
+                      pretrained_weights_filename = 'models/pretrained/mnist_fstlay_uni_const_lenet_w_acc_95.pt',
+                      pretrained_unitary_matrix_filename = 'models/pretrained/U_mnist_fstlay_uni_const_lenet_w_acc_95.pt')
+
+# Initialize attackers
+lenet_attacker = Attacker(lenet, data)
+Unet_attacker = Attacker(Unet, data)
+
+# Compare Eigensystems
+table = PrettyTable(hrules = ALL)
+table.field_names = ["Image Label", "LeNet Eigenvalue", "LeNet Eigenvector Head",
+                                    "U-Net Eigenvalue", "U-Net Eigenvector Head"]
+
+for image_index in range(num_images):
+    lenet_eig_val_max, lenet_eig_vec_max = lenet_attacker.get_single_OSSA_attack(image_index = image_index,
+                                                                                 eigens_only = True)
+
+    Unet_eig_val_max, Unet_eig_vec_max = Unet_attacker.get_single_OSSA_attack(image_index = image_index,
+                                                                              eigens_only = True)
+
+    # Convert to Scientific Notation
+    lenet_eig_val_max = "{:.2e}".format(lenet_eig_val_max.item())
+    Unet_eig_val_max = "{:.2e}".format(Unet_eig_val_max.item())
+    lenet_eig_vec_head = ["{:.2e}".format(x) for x in lenet_eig_vec_max.squeeze().squeeze().tolist()[0:eig_vec_head_size]]
+    Unet_eig_vec_head = ["{:.2e}".format(x) for x in Unet_eig_vec_max.squeeze().squeeze().tolist()[0:eig_vec_head_size]]
+
+    # Load Table
+    for j in range(eig_vec_head_size  + 1):
+        if j == 0:
+            table.add_row([image_index, lenet_eig_val_max, lenet_eig_vec_head[j], 
+                                        Unet_eig_val_max,  Unet_eig_vec_head[j]])
+        elif j != eig_vec_head_size:
+            table.add_row(["-", "-", lenet_eig_vec_head[j], "-", Unet_eig_vec_head[j]])
+        else:
+            table.add_row(["-", "-", ":" , "-", ":"])
+            table.add_row([("\\"*int(np.floor(len(table.field_names[i])/2))) + ("/"*int(np.floor(len(table.field_names[i])/2))) for i in range(len(table.field_names))])
+
+print(table)
---
+++ mixture_model_trainer.py
@@ -0,0 +1,66 @@
+# Imports
+from models.classes.first_layer_unitary_lenet import FstLayUniLeNet
+from data_setup import Data
+from academy import Academy
+import numpy as np
+import torch
+
+# Hyperparameters
+gpu         = False
+save_model  = True
+n_epochs    = 2000
+set_name    = "MNIST"
+seed        = 100
+reg_train_ratio = 0.5
+
+# Push to GPU if necessary
+if gpu == True:
+    import os
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+# Declare seed and initalize network
+net = FstLayUniLeNet(set_name = set_name,
+                    seed = seed,
+                    U = None)
+U = net.get_orthogonal_matrix(784)
+
+
+# Load data
+data = Data(gpu = gpu, set_name = "MNIST")
+
+# Enter student network and curriculum data into an academy
+academy  = Academy(net, data, gpu)
+
+# Swich between U and Reg
+for _ in range(n_epochs):
+    if reg_train_ratio < np.random.uniform():
+        academy.net.U = None
+    else:
+        academy.net.U = U
+
+    # Fit Model
+    academy.train(n_epochs = 1)
+
+# Calculate accuracy on test set
+academy.net.U = None
+reg_accuracy  = academy.test()
+print("Regular Network Accuracy: ", reg_accuracy)
+
+academy.net.U = U
+U_accuracy  = academy.test()
+print("Unitary Network Accuracy: ", U_accuracy)
+accuracy = (reg_accuracy + U_accuracy)/2
+print("Average Network Accuracy: ", accuracy)
+
+# Save Model
+if save_model:
+    # Define File Names
+    filename  = "mixed_U_net_w_acc_" + str(int(round(accuracy * 100, 3))) + ".pt"
+    
+    # Save Models
+    torch.save(academy.net.state_dict(), filename)
+
+    # Save U
+    if net.U is not None:
+        torch.save(net.U, "U_for_" + filename)
