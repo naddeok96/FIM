25926fc44d1dc24ff1468ff9a3dbea3f74d5a58c
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-31 15:47:38 -0700

    added control test

--- transfer_attacks.py
+++ transfer_attacks.py
@@ -47,10 +47,21 @@ attacker = Attacker(attacker_lenet,
                     data, 
                     gpu)
 
-# Get attack accuracies for Lenet
+# Get regular attack accuracies on attacker network
+print("Working on OSSA Attacks...")
+ossa_accs  = attacker.get_OSSA_attack_accuracy(epsilons = epsilons)                                          
+ossa_fool_ratio = attacker.get_fool_ratio(0.98, ossa_accs)
+table.add_column("OSSA Fool Ratio", ossa_fool_ratio)
+
+print("Working on FGSM Attacks...")
+fgsm_accs = attacker.get_FGSM_attack_accuracy(epsilons = epsilons)
+fgsm_fool_ratio = attacker.get_fool_ratio(0.98, fgsm_accs)
+table.add_column("FGSM Attack Accuracy", fgsm_fool_ratio)
+
+# Get transfer attack accuracies for Lenet
 print("Working on LeNet OSSA Attacks...")
 lenet_ossa_accs  = attacker.get_OSSA_attack_accuracy(epsilons = epsilons,
-                                                     transfer_network = lenet)                                              
+                                                     transfer_network = lenet)                                          
 lenet_ossa_fool_ratio = attacker.get_fool_ratio(0.98, lenet_ossa_accs)
 table.add_column("LeNet OSSA Fool Ratio", lenet_ossa_fool_ratio)
 
@@ -61,7 +72,7 @@ lenet_fgsm_fool_ratio = attacker.get_fool_ratio(0.98, lenet_fgsm_accs)
 table.add_column("LeNet FGSM Attack Accuracy", lenet_fgsm_fool_ratio)
 
 
-# Get attack accuracies for Unet
+# Get transfer attack accuracies for Unet
 print("Working on UNet OSSA Attacks...")
 Unet_ossa_accs  = attacker.get_OSSA_attack_accuracy(epsilons = epsilons,
                                                      transfer_network = Unet)                                              
@@ -90,8 +101,12 @@ if save_to_excel:
     sheet = wb.add_sheet('Results') 
 
     # Write out each peak and data
-    results = [epsilons, ossa_fool_ratio, U_eta_fool_ratio, fgsm_fool_ratio]
+    results = [epsilons, 
+               ossa_fool_ratio, fgsm_fool_ratio,
+               lenet_ossa_fool_ratio, lenet_fgsm_fool_ratio,
+               Unet_ossa_fool_ratio, Unet_fgsm_fool_ratio]
     names = ["epsilons", 
+             "ossa_fool_ratio", "fgsm_fool_ratio",
              "lenet_ossa_fool_ratio", "lenet_fgsm_fool_ratio", 
              "Unet_ossa_fool_ratio", "Unet_fgsm_fool_ratio"]
     for i, result in enumerate(results):
