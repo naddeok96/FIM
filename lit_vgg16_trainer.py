f8ca7becaf4e75004d713a28a47579e7168ca4d8
Kyle Naddeo <naddeok5@students.rowan.edu>
2021-03-16 10:47:31 -0400

    testing orthognal mat setup

--- lit_vgg16_trainer.py
+++ lit_vgg16_trainer.py
@@ -6,21 +6,24 @@ import torch
 import namegenerator
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
-from models.classes.lightning_vgg16 import LitVGG16
+from models.classes.lit_vgg16 import LitVGG16
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 # Parameters
 wandb_project_name = 'VGG16 ImageNet Lightning'
+wandb_mode         = "offline" # "online", "offline" or "disabled"
+save_k_models      = 1
 run_name = "standard_U"
-gpus = "0,1,2,3"
-n_epochs = 1000
+gpus = "2"
+n_epochs = 0
 batch_size = 1
 pretrained = False
 
-
 # Initalize Weights and Biases
-wandb_logger = WandbLogger(name = run_name, project=wandb_project_name)
+wandb_logger = WandbLogger(name = run_name, 
+                           project=wandb_project_name,
+                           mode = wandb_mode)
 
 # Initalize Model
 net = LitVGG16(batch_size=batch_size,
@@ -30,14 +33,17 @@ net = LitVGG16(batch_size=batch_size,
 # Setup Orthoganal Matrix
 # net.load_orthogonal_matrix("models/pretrained/high_R_U.pkl")
 net.set_orthogonal_matrix()
+exit()
 # net.set_random_matrix()
 net.save_orthogonal_matrix("models/pretrained/LeNet_MNIST_" + run_name + ".pkl")
+
+exit()
 
 # Setup Automatic Saving
 checkpoint_callback = ModelCheckpoint(
     dirpath='/home/naddeok5/FIM/models/pretrained/',
     filename='Lit_VGG16_ImageNet_' + run_name + '-{val_acc:.2f}',
-    save_top_k = 1,
+    save_top_k = save_k_models,
     verbose=False,
     monitor='train_loss',
     mode='min',
