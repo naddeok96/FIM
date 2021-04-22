77de0c6f31eb1cd08249d3d2e177c4e9566754de
Kyle <naddeok5@students.rowan.edu>
2021-04-22 11:50:52 -0400

    initial commit

--- view_transfer_attack_results.py
+++ view_transfer_attack_results.py
@@ -1,5 +1,22 @@
 import xlrd
+import matplotlib.pyplot as plt
+
 
 wb = xlrd.open_workbook("results/MNIST/transfer_attack_results.xls")
 ws = wb.sheet_by_name("Results")
 
+results = {}
+for col in range(len(ws.row(0))):
+    results.update({ws.cell(0, col).value: []})
+
+    for row in range(1, len(ws.col(col))):
+        results[ws.cell(0, col).value].append(ws.cell(row, col).value)
+        
+for key, value in results.items():
+    if key == "Epsilons":
+        continue
+
+    plt.plot(results["Epsilons"], value, '.-', label=key)
+    
+plt.legend()
+plt.show()
