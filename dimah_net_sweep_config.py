fc49dcbaefedba10ae362a8475eb3ea4268fa864
Kyle Naddeo <naddeok96@github.com>
2021-05-07 13:09:53 -0400

    initial commit

---
+++ dimah_net_sweep_config.py
@@ -0,0 +1,34 @@
+sweep_config = {
+                'method': 'bayes',
+                'metric': {
+                    'name': 'loss',
+                    'goal': 'minimize'   
+                },
+                'parameters': {
+                    'epochs': {
+                        'values': [1e2, 1e3]
+                    },
+                    'batch_size': {
+                        'values': [64, 124]
+                    },
+                    'momentum': {
+                        'values': [0.9]
+                    },
+                    'weight_decay': {
+                        'values': [1e-3]
+                    },
+                    'learning_rate': {
+                        'values': [1e-2]
+                    },
+                    'optimizer': {
+                        'values': ['adadelta']
+                    },
+                    'criterion': {
+                        'values': ['mse', 'cross_entropy']  
+                    },
+                    'use_SAM':{
+                        'values' : [False]
+                    }
+                }
+            }
+             
