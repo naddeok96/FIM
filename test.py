f42b7c26d485b62eace7cd9ef7427b6d79224ef5
Kyle Naddeo <naddeok5@students.rowan.edu>
2021-06-07 19:59:28 -0400

    misc changes

--- test.py
+++ test.py
@@ -2,120 +2,69 @@ from data_setup import Data
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from models.classes.first_layer_unitary_resnet    import FstLayUniResNet
+import torch.nn as nn
+import torch.optim as optim
+from models.classes.first_layer_unitary_net    import FstLayUniNet
 import torchvision
 import torchvision.transforms as transforms
-
-def imshow(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-    for i in range(img.size(1)):
-        
-        print("Before: ", torch.min(img[:, i, :, :]), torch.max(img[:, i, :, :]))
-        img[:, i, :, :] = (img[:, i, :, :] - torch.min(img[:, i, :, :])) / (torch.max(img[:, i, :, :]) - torch.min(img[:, i, :, :]))
-        print("After: ", torch.min(img[:, i, :, :]), torch.max(img[:, i, :, :]))
-    return img
+import os
+import art
+from art import config
+from art.estimators.classification import PyTorchClassifier
 
 # Hypers
 set_name = 'CIFAR10'
 batch_size = int(5e4)
+gpu = True
+batch_size = 256
+
+# Push to GPU if necessary
+if gpu:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+# Load Data
+config.set_data_path('../../../data/pytorch/CIFAR10')
+(x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = load_cifar10()
+# data = Data(gpu = gpu, set_name = set_name, maxmin = True, test_batch_size = batch_size)
+# images, labels = next(iter(data.test_loader))
+# images, labels = images.detach().numpy(), labels.detach().numpy()
+print(set_name + " is Loaded")
 
 # Load Network
-net = FstLayUniResNet(gpu = False, set_name = set_name,
+net = FstLayUniNet(set_name = set_name, gpu =gpu,
                        model_name = 'cifar10_mobilenetv2_x1_0',
-                       pretrained = False)
-# Set a random orthogonal matrix
-net.set_orthogonal_matrix()
+                       pretrained = True)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.01)
 
-# Load Standard CIFAR10
-transform = transforms.Compose([transforms.ToTensor(), # Convert the images into tensors
-                                  transforms.Normalize((0.4914, 0.4822, 0.4465), 
-                                                       (0.2023, 0.1994, 0.2010))
-                                                        ]) #  Normalize
-dataset = torchvision.datasets.CIFAR10(root='../data', # '../../../data/pytorch/CIFAR10', # 
-                                        train=True,
-                                        download=True,
-                                        transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size = batch_size)
+classifier = PyTorchClassifier(
+    model=net,
+    clip_values=(data.test_pixel_min, data.test_pixel_max),
+    loss=criterion,
+    optimizer=optimizer,
+    input_shape=(data.num_channels, data.image_size, data.image_size),
+    nb_classes=data.num_classes,
+)
+
+
+predictions = classifier.predict(images)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(labels, axis=0)) / len(labels)
+print("Accuracy on benign test examples: {}%".format(accuracy * 100))
+
+
+# Load adverasy
+# art.attacks.evasion.CarliniL2Method(classifier = net, 
+#                                     confidence =  0.0,
+#                                     learning_rate = 0.01, 
+#                                     binary_search_steps = 10, 
+#                                     max_iter = 10,
+#                                     initial_const = 0.01, 
+#                                     max_halving = 5,
+#                                     max_doubling  = 5, 
+#                                     batch_size = 1,
+#                                     verbose= True)
 
 # Collect Stats
-for images, labels in dataloader:
-    # Rotate Images
-    ortho_images = net.orthogonal_operation(images)
-
-    # Resize
-    ortho_images = ortho_images.view(ortho_images.size(0), ortho_images.size(1), -1)
+# for images, labels in dataloader:
     
-    # Calulate Stats
-    means = ortho_images.mean(2).mean(0)
-    stds  = ortho_images.std(2).mean(0)
-
-    # # Normalize Images
-    # batch_means = means.repeat(ortho_images.size(0), 1).view(ortho_images.size(0), ortho_images.size(1), 1)
-    # batch_stds  = stds.repeat(ortho_images.size(0), 1).view(ortho_images.size(0), ortho_images.size(1), 1)
-    # norm_ortho_images = ortho_images.sub_(batch_means).div_(batch_stds).view(ortho_images.size(0), ortho_images.size(1), 32, 32)
-
-    # All images should be loaded in one batch
-    break
-
-# Generate Saving name with stats encoded
-filename = "U_w_means_"
-for mean in means:
-    strmean = str(mean.item())
-    # print(strmean)
-    for c in strmean:
-        if c == "-":
-            filename += "n"
-        elif c == ".":
-            filename += "-"
-        else:
-            filename += c
-    filename += "_"
-
-filename += "and_stds_"
-for std in stds:
-    strstd = str(std.item())
-    for c in strstd:
-        if c == "-":
-            filename += "n"
-        elif c == ".":
-            filename += "-"
-        else:
-            filename += c
-    filename += "_"
-filename += ".pt"
-print(filename)
-torch.save(net.U, "models/pretrained/" + filename)
-
-
-# # Decode filename
-# stats = ["", "", "", "", "", ""]
-# state = 0
-# sofar = ""
-# for c in filename:        
-#     sofar += c
-#     if sofar == "U_w_means_":
-#         state = 1
-#         continue
-    
-#     if state == 4 and "and_stds_" not in sofar:
-#         continue
-
-#     if state == 4 and len(stats[state - 1]) == 0 and c == "_":
-#         continue
-
-#     if 0 < state and state < 7:
-#         if c == "n":
-#             stats[state - 1] += "-"
-#         elif c == "-":
-#             stats[state - 1] += "."
-#         elif c == "_":
-#             stats[state - 1] = float(stats[state-1])
-#             state += 1
-#         else: 
-#             stats[state - 1] += c
-
-# means = torch.tensor(stats[0:3])
-# stds  = torch.tensor(stats[3:])
