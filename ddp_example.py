253b74f60430e52816baddd046064ad53d3c5a12
Kyle Naddeo <naddeok96@github.com>
2021-09-17 12:34:38 -0400

    working ddp_example

--- ddp_example.py
+++ ddp_example.py
@@ -14,13 +14,13 @@ from models.classes.first_layer_unitary_net  import FstLayUniNet
 from torch.nn.parallel import DistributedDataParallel as DDP
 from mnist_sweep_config import sweep_config
 
-def setup(rank, world_size):
+def setup(rank, world_size, use_wandb):
     # Setup rendezvous
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
     # Initalize WandB logging on rank 0
-    if rank == 0:
+    if rank == 0 and use_wandb:
         run = wandb.init(
                 entity  = "naddeok",
                 project = "DDP MNIST")
@@ -29,7 +29,7 @@ def setup(rank, world_size):
 
     # Initialize the process group
     dist.init_process_group(backend    = "nccl",
-                        #    init_method = "env://", 
+                           init_method = "env://", 
                            rank        = rank, 
                            world_size  = world_size)
     return run
@@ -38,10 +38,10 @@ def cleanup():
     # Close all processes
     dist.destroy_process_group()
 
-def train(rank, world_size, epochs, batch_size):
+def train(rank, world_size, epochs, batch_size, use_wandb):
     # Initialize WandB and Process Group
     print(f"Training on rank {rank}.")
-    run = setup(rank, world_size)
+    run = setup(rank, world_size, use_wandb)
 
     # Create model and move it to GPU with id rank
     model = FstLayUniNet(   set_name = "MNIST", 
@@ -61,8 +61,10 @@ def train(rank, world_size, epochs, batch_size):
     optimizer = torch.optim.SGD(model.parameters(), 1e-4)
 
     # Data loading code
-    train_dataset = torchvision.datasets.MNIST(
-        root='../../../data/pytorch/', train=True, transform=transforms.ToTensor(), download=False)
+    train_dataset = torchvision.datasets.MNIST(root='../../../data/pytorch/', 
+                                                train=True, 
+                                                transform=transforms.ToTensor(), 
+                                                download=False)
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
 
@@ -105,36 +107,39 @@ def train(rank, world_size, epochs, batch_size):
             run.log({"epoch": epoch, "loss": np.mean(batch_loss)})
 
     
-    # Shut down gpu processes
-    cleanup()
+    # End of training
     print("Done Training on Rank ", rank)
 
-    if rank == 0:
-        dist.barrier()
+    # Shut down gpu processes
+    dist.barrier()
+    cleanup()
+
+    # Close WandB
+    if rank == 0 and run is not None:
         wandb.finish()
+        print("WandB Finished")
+
+    
     
 
-def run_ddp(func, world_size, epochs, batch_size):
+def run_ddp(func, world_size, epochs, batch_size, use_wandb):
 
     # Spawn processes on gpus
     mp.spawn(func,
-             args=(world_size, epochs, batch_size,),
+             args=(world_size, epochs, batch_size, use_wandb,),
              nprocs=world_size,
              join=True)
-
-    
 
     # Display
     print("Run Complete")
 
-    
-
 if __name__ == "__main__":
     # Hyperparameters
-    gpu_ids      = "4, 5"
+    gpu_ids      = "2, 6"
     project_name = "DDP MNIST"
     epochs       = 2
     batch_size   = int(2**9)
+    use_wandb    = True
 
     # Set GPUs to Use
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -144,4 +149,4 @@ if __name__ == "__main__":
     assert n_gpus >= 2, f"Requires at least 2 GPUs to run, but got {n_gpus}"
 
     # Run training using DDP
-    run_ddp(train, n_gpus, epochs, batch_size)
+    run_ddp(train, n_gpus, epochs, batch_size, use_wandb)
